cea90a0d4ba181072c3274316f9c7affd3fda4c4
Kleber Silva <krsmga@gmail.com>
2021-11-27 23:21:47 -0300

    first commit

---
+++ src/pages/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export function Button({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      activeOpacity={0.8}
+      onPress={onPress}>
+      <Text style={styles.buttontext}>Add</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttontext: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
